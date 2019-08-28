1494c50a48e9c95c70c82bb30420a520a73ccc5c
dev1980 <brham1980@gmail.com>
2019-08-29 02:49:21 +0545

    my_each method added

---
+++ Enumerable_bubble_sort.rb
@@ -0,0 +1,13 @@
+module Enumerable
+    def my_each
+        i = 0
+        while i < self.length
+            yield(self[i])
+            i += 1
+        end
+        return self
+    end
+
+   
+    
+end
