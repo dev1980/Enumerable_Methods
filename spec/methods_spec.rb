287baa910e74eb0206aeb6fbe7cb49b4245ebc6f
dev1980 <brham1980@gmail.com>
2019-09-03 02:31:42 +0545

    completed 4 methods testing

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -6,10 +6,28 @@ require './methods.rb'
 
 RSpec.describe Enumerable do
   let(:test_array_1) { [1, 2, 3, 4, 5] }
+  let(:test_array_2) { [3] }
 
   describe '#my_each' do
     it 'returns all elements in the array' do
       expect(test_array_1.my_each { |e| e }).to eql(test_array_1)
+    end
+  end
+   
+  describe '#my_each_with_index' do
+    it 'returns all elements with the index in the array' do
+      expect(test_array_1.my_each_with_index { |_e, i| i }).to eql(test_array_1)
+    end
+  end
+  describe '#my_select' do
+    it 'returns an array depending on the condition' do
+      expect(test_array_1.my_select { |e| e % 3 == 0 }).to eql(test_array_2)
+    end
+  end
+  
+  describe '#my_all' do
+    it 'returns true if all the elements meet the condition' do
+      expect(test_array_1.my_all? { |e| e % 3 == 0 }).to eql(false)
     end
   end
 
